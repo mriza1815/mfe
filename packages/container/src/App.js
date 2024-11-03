bd715d6c14d94cc3a4491445289a44242ced4318
riza.rizaoglu <riza.rizaoglu@obss.tech>
2024-11-03 14:21:09 +0300

    deploy test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
     return (
         <div>
-            <h1>Hello hello mate!</h1>
+            <h1>Hello mate!</h1>
             <hr/>
             <MarketingApp/>
         </div>
