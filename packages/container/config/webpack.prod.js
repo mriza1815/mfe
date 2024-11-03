a095b97ddd0ce7cbb71fa2faba1b024b4b5bdb2f
riza.rizaoglu <riza.rizaoglu@obss.tech>
2024-11-03 15:08:48 +0300

    deploy test, webpack publicPath option added

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -11,13 +11,15 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: "production",
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/' //HTML Webpack Plugin html dosyasına script'leri yerleştirmeden önce "filename" tipinde scripttleri oluşturup 
+        //publicPath kısmının altına yerleştirir. Bu sayede proje deploy olduğu zaman script tag'leri doğru path'e sahip olur
     },
     plugins: [
         new ModuleFederationPlugin({
             name: "container",
             remotes: {
-                marketing : `marketing@${domain}/marketing/remoteEntry.js`,
+                marketing : `marketing@${domain}/marketing/latest/remoteEntry.js`,
             },
             shared: packageJson.dependencies
         })
