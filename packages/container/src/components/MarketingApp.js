30f71c97f3434c32f773fe8136e9e82f51e77c19
riza.rizaoglu <riza.rizaoglu@obss.tech>
2024-11-03 19:48:10 +0300

    navigation and communication beetween container andmarketing mfe

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,28 @@
 import { mount } from "marketing/MarketingApp"
 import React, { useEffect, useRef } from "react"
+import { useHistory } from "react-router-dom"
 
 export default () => {
     const ref = useRef(null)
+    const history = useHistory() // Browser History object
 
     useEffect(() => {
-        mount(ref.current)
-    })
+        const { onParentNavigate } = mount(ref.current, {
+            initialPath: history.location.pathname,
+            // remote -> host(child to parent) navigation communication via callback
+            onNavigate: ({pathname: nextPathname}) => {
+                const { pathname } = history.location
+                
+                if(pathname !== nextPathname){
+                    console.log("remote navigated", nextPathname)
+                    history.push(nextPathname)
+                }
+            }
+        })
+
+        // host -> remote(parent to child) navigation communication via callback
+        history.listen(onParentNavigate)
+    }, [])
 
     return <div ref={ref} ></div>
 }
